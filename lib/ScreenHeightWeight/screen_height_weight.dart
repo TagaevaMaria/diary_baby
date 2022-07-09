fa1a708e2fd5ee966c6855ba239ba502ee9ea427
Masha <vcebudet5.okyy@gmail.com>
2022-07-10 00:49:42 +0700

    [dev]экран рост и вес ребенка

--- lib/ScreenHeightWeight/screen_height_weight.dart
+++ lib/ScreenHeightWeight/screen_height_weight.dart
@@ -10,61 +10,99 @@ class ScreenHeightWeight extends StatefulWidget {
 }
 
 class _ScrenHeightWeightState extends State<ScreenHeightWeight> {
-  ///лист событий
-  List _listEvent = [];
-
-  ///элменты списка прописываются в специальном методе initState
-  @override
-  void initState() {
-    super.initState();
-
-    _listEvent.addAll(['рост:104, вес: 4100']);
-  }
-
   @override
   Widget build(BuildContext context) {
     return BackgroundWidget(
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(165, 218, 249, 1),
-          title: Text('Показатели'),
-        ),
-        body: ListView.builder(
-            itemCount: _listEvent.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Dismissible(
-                key: Key(_listEvent[index]),
-                child: Card(
-                  child: ListTile(
-                    title: Text(_listEvent[index]),
-                  ),
-                ),
-                onDismissed: (direction) {
-                  setState(() {
-                    _listEvent.removeAt(index);
-                  });
-                },
-              );
-            }),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text('Показатели'),
-                    content: TextField(
-                      onChanged: (String value) {
-                        return;
-                      },
-                    ),
-                  );
-                });
-          },
-          child: Icon(Icons.add),
+        child: Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(165, 218, 249, 1),
+        title: Text('Показатели'),
+      ),
+
+      /// кнопки вес и рост
+      body: _rowWidgets(
+        title: 'вес',
+        titleTwo: 'рост',
+        image: 'image/image_screen_ height_weight/weight.png',
+        imageTwo: 'image/image_screen_ height_weight/height.png',
+        size: 80.0,
+        paddingOne: EdgeInsets.only(top: 50, bottom: 20, left: 10, right: 10),
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        onPressed: () {},
+        onPressedTwo: () {},
+        paddingTwy: EdgeInsets.only(top: 50, bottom: 20, left: 10, right: 10),
+      ),
+    ));
+  }
+}
+
+/// Ряд виджетов.
+Widget _rowWidgets({
+  required String title,
+  required String titleTwo,
+  required String image,
+  required String imageTwo,
+  required double size,
+  required EdgeInsets paddingOne,
+  required EdgeInsets paddingTwy,
+  required MainAxisAlignment mainAxisAlignment,
+  required CrossAxisAlignment crossAxisAlignment,
+  required Function() onPressed,
+  required Function() onPressedTwo,
+}) {
+  return Row(
+    mainAxisAlignment: mainAxisAlignment,
+    crossAxisAlignment: crossAxisAlignment,
+    children: [
+      _circleButtonWidget(
+        title: title,
+        image: image,
+        size: size,
+        padding: paddingOne,
+        onPressed: onPressed,
+      ),
+      _circleButtonWidget(
+        title: titleTwo,
+        image: imageTwo,
+        size: size,
+        padding: paddingTwy,
+        onPressed: onPressedTwo,
+      ),
+    ],
+  );
+}
+
+/// Виджет кнопки выбора действия.
+Widget _circleButtonWidget({
+  required String title,
+  required String image,
+  required double size,
+  required EdgeInsets padding,
+  required Function() onPressed,
+}) {
+  return Column(
+    children: [
+      TextButton(
+        onPressed: onPressed,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 16, bottom: 20, left: 10, right: 10),
+          child: Image(
+            image: AssetImage(image),
+            width: size,
+            height: size,
+          ),
         ),
       ),
-    );
-  }
+      Padding(
+        padding: padding,
+        child: Text(
+          title,
+          style: const TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+        ),
+      )
+    ],
+  );
 }
